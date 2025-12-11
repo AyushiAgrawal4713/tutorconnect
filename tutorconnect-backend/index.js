43760c73614a94025481033d9ab1bfff47a96471
Ayushi Agrawal <agrawalayushi683@gmail.com>
2025-12-11 11:38:36 +0530

    chore: setup Tailwind + backend initialization

--- tutorconnect-backend/index.js
+++ tutorconnect-backend/index.js
@@ -10,5 +10,6 @@ app.get("/", (req, res) => {
   res.send("Backend running");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5001;
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
